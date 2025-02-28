1302311aed7206ff112e4ab9ebf36705e95b5a75
propulsion86 <wmmpererawtuta.io>
2025-03-01 11:32:18 +1300

    new webserver init

---
+++ gallery-server/models/models.go
@@ -0,0 +1,16 @@
+package models
+
+type ImageMeta struct {
+	FilePath     string
+	Description  string
+	ISO          string
+	ShutterSpeed string
+	Aperture     string
+	Location     string
+}
+
+type User struct {
+	Username string
+	Password string
+	admin    bool
+}
